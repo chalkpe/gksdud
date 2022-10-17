e44f7ff8b5147bbe3112b60ae022c1a78ad67861
Wendrul <dethomassin.etienne@gmail.com>
2022-10-17 18:02:01 +0200

    Add unit tests

--- test/gksdud.js
+++ test/gksdud.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 
 describe('gksdud', function(){
     const gksdud = require('../lib/gksdud');
+    const reverseGksdud = require('../lib/gksdud').reverseGksdud;
 
     it('works with basic words', function(){
         expect(gksdud('DNS')).to.be.equal('운');
@@ -47,4 +48,64 @@ describe('gksdud', function(){
         expect(gksdud('dlTsms rj djqtsms rj ahdEkd rmfrdj ahdmwk')).to.be.equal('있는 거 없는 거 몽땅 긁어 모으자');
         expect(gksdud('wha Ejfqdms smRladl Wkfqwlaks rkdgkrp smRuwuTek')).to.be.equal('좀 떫은 느낌이 짧지만 강하게 느껴졌다');
     });
+
+    /** Reverse gksdud */
+
+    it('works with reversing basic words', function(){
+        expect(reverseGksdud('운')).to.be.equal('dns');
+        expect(reverseGksdud('듀냐')).to.be.equal('ebsi');
+        expect(reverseGksdud('낯이 참 좋네')).to.be.equal('skcdl cka whgsp');
+        expect(reverseGksdud('예의를 갖추세요')).to.be.equal('dPdmlfmf rkwcntpdy');
+        expect(reverseGksdud('한영키가 늘 문제야')).to.be.equal('gksdudzlrk smf answpdi');
+        expect(reverseGksdud('동해 물과 백두산이')).to.be.equal('ehdgo anfrhk qorentksdl');
+        expect(reverseGksdud('베재댜겨쇼미나어롷크투츞')).to.be.equal('qpwoeirutyalskdjfhgzmxncbv');
+        expect(reverseGksdud('얘기를 들어도 모르겠는데')).to.be.equal('dOrlfmf emfdjeh ahfmrpTsmsep');
+    });
+
+    it('works with reversing jamo-only words', function(){
+        expect(reverseGksdud('ㅗㅜㅑ')).to.be.equal('hni');
+        expect(reverseGksdud('ㅣㅔㅏㅣㅔ')).to.be.equal('lpklp');
+        expect(reverseGksdud('ㄱㄹㅅ ㅇㅈㄹ ㅍㅁㄹ ㅎㅅㄱㄷ?')).to.be.equal('rft dwf vaf gtre?');
+        expect(reverseGksdud('ㅇㅈㄱㅇ ㅈㄹㄱㅇ ㅇㄱㄹㅇ ㅂㅂㅂㄱ')).to.be.equal('dwrd wfrd drfd qqqr');
+    });
+
+    it('works with reversing complex inital jamos', function(){
+        expect(reverseGksdud('빵상 깨랑깨랑')).to.be.equal('Qkdtkd RofkdRofkd');
+        expect(reverseGksdud('짜장면이나 시켜 먹자')).to.be.equal('Wkwkdausdlsk tlzu ajrwk');
+        expect(reverseGksdud('씨프트키까 안 빠쪄요 ㅠㅠ')).to.be.equal('TlvmxmzlRk dks QkWudy bb');
+    });
+
+    it('works with reversing isolated complex jamo', function() {
+        expect(reverseGksdud("ㅘㄻㄺ")).to.be.equal('hkfafr');
+        expect(reverseGksdud("ㅋㅋㅋㅋㅙㅙㅋㅋ")).to.be.equal('zzzzhohozz');
+        expect(reverseGksdud("ㅙㅣㅁ 볃 ㅅ미?")).to.be.equal('hola que tal?');
+    });
+
+    it('works with reversing complex medial jamos', function(){
+        expect(reverseGksdud('아이어를 위하여!')).to.be.equal('dkdldjfmf dnlgkdu!');;
+        expect(reverseGksdud('왜 그렇게 생각했습니까?')).to.be.equal('dho rmfjgrp todrkrgoTtmqslRk?');
+        expect(reverseGksdud('이 때의 원리합계를 구하시오.')).to.be.equal('dl Eodml dnjsflgkqrPfmf rngktldh.');
+        expect(reverseGksdud('이 외진 곳에 웬일로 찾아왔어?')).to.be.equal('dl dhlwls rhtdp dnpsdlffh ckwdkdhkTdj?');
+    });
+
+    it('works with reversing complex final jamos', function(){
+        expect(reverseGksdud('좀 깎아 주시죠?')).to.be.equal('wha RkRdk wntlwy?');
+        expect(reverseGksdud('싫지는 않은데...')).to.be.equal('tlfgwlsms dksgdmsep...');
+        expect(reverseGksdud('여기 품삯이 참 후하네')).to.be.equal('durl vnatkrtdl cka gngksp');
+        expect(reverseGksdud('아이스크림 핥아 먹어야지')).to.be.equal('dkdltmzmfla gkfxdk ajrdjdiwl');
+        expect(reverseGksdud('서 있지 말고 좀 앉아 봐요')).to.be.equal('tj dlTwl akfrh wha dkswdk qhkdy');
+        expect(reverseGksdud('아들이 엄마를 많이 닮았네')).to.be.equal('dkemfdl djaakfmf aksgdl ekfadkTsp');
+        expect(reverseGksdud('40년 동안 외곬으로 걸어왔다')).to.be.equal('40sus ehddks dhlrhftdmfh rjfdjdhkTek');
+        expect(reverseGksdud('서당 개 3년이면 풍월을 읊는다')).to.be.equal('tjekd ro 3susdlaus vnddnjfdmf dmfvsmsek');
+        expect(reverseGksdud('동해 물과 백두산이 마르고 닳도록')).to.be.equal('ehdgo anfrhk qorentksdl akfmrh ekfgehfhr');
+        expect(reverseGksdud('있는 거 없는 거 몽땅 긁어 모으자')).to.be.equal('dlTsms rj djqtsms rj ahdEkd rmfrdj ahdmwk');
+        expect(reverseGksdud('좀 떫은 느낌이 짧지만 강하게 느껴졌다')).to.be.equal('wha Ejfqdms smRladl Wkfqwlaks rkdgkrp smRuwuTek');
+    });
+
+    it('works with reversing typos when trying to write roman characters', function(){
+        expect(reverseGksdud('ㅗ디ㅣㅐ ㄹ갸둥, ㅑ 므 ㅔㅣㄷㅁㄴㄷㅇ 새 ㅡㄷㄷㅅ ㅛㅐㅕ!')).to.be.equal('hello friend, i am pleased to meet you!');;
+        expect(reverseGksdud('ㅎㅅㅎ, ㅅ쇼ㅣ')).to.be.equal('gtg, ttyl');
+        expect(reverseGksdud('댤ㄹ디 샞ㄷㄱ ㅜㅑ홋 ㅣㅑ홋ㄴ')).to.be.equal('eiffel tower night lights');
+    });
+    
 });
